66689827423db2e4615937cc78e870bd63e4e2bc
Luis Pedro Coelho <luis@luispedro.org>
2014-11-22 12:44:18 +0100

    ENH Allow multiple targets

--- beemind/main.py
+++ beemind/main.py
@@ -1,28 +1,29 @@
 from time import time
 import re
-from config import BEEMIND_DATA_FILE, BEEMIND_GOAL, BEEMIND_AUTH_TOKEN
+from config import BEEMIND_DATA_FILE, BEEMIND_AUTH_TOKEN
            
-pat = re.compile('^(start|stop): ([0-9]+\.[0-9]+)$')
+pat = re.compile(r'^(start|stop) \[(\w+)\]: ([0-9]+\.[0-9]+)$')
 
 def parse_files(inputf):
-    total = 0
-    startp = None
+    from collections import defaultdict
+    total = defaultdict(float)
+    startp = {}
     for line in inputf:
         m = pat.match(line)
         if m is None:
             raise ValueError("Could not parse line {}".format(line))
-        code,timestamp = m.groups()
+        code,goal,timestamp = m.groups()
         timestamp = float(timestamp)
         if code == 'start':
-            if not startp:
-                startp = timestamp
+            if goal not in startp:
+                startp[goal] = timestamp
         elif code == 'stop':
-            if startp:
-                total += timestamp - startp
-                startp = False
-    if startp:
-        total += time() - startp
-    return total
+            if goal in startp:
+                total[goal] += timestamp - startp[goal]
+                del startp[goal]
+    for k,v in startp.items():
+        total[k] += time() - v
+    return dict(total)
 
 def build_post_data(auth_token, goal, value, comment):
     from urllib.parse import urlencode
@@ -45,21 +46,50 @@ def post_data(auth_token, goal, value, comment):
     print(output)
     return output
 
+def fsync_all(fnames):
+    import os
+    for f in fnames:
+        fd = None
+        try:
+            fd = os.open(f, os.O_RDONLY)
+            os.fsync(fd)
+        finally:
+            if fd is not None:
+                os.close(fd)
+
+def remove_goal_data(inputf, target):
+    from os import rename
+    with open(inputf) as orig:
+        with open(inputf + '.tmp', 'w') as tmp_file:
+            for line in orig:
+                _,goal,_ = pat.match(line).groups()
+                if goal != target:
+                    tmp_file.write(line)
+    fsync_all([inputf + '.tmp'])
+    rename(inputf + '.tmp', inputf)
+
+
 def main(argv):
     now = time()
-    if argv[1] == 'start':
+    goal = argv[1]
+    action = argv[2]
+    if action == 'start':
         with open(BEEMIND_DATA_FILE, 'a+') as output:
-            output.write('start: {}\n'.format(now))
-    elif argv[1] == 'stop':
+            output.write('start [{}]: {}\n'.format(goal, now))
+    elif action == 'stop':
         with open(BEEMIND_DATA_FILE, 'a+') as output:
-            output.write('stop: {}\n'.format(now))
-    elif argv[1] == 'submit':
-        from os import unlink
+            output.write('stop [{}]: {}\n'.format(goal, now))
+    elif action == 'status':
         total = parse_files(open(BEEMIND_DATA_FILE))
+        for k,tm in total.items():
+            tm /= 3600.
+            print('{:16}\t{:.3}'.format(k,tm))
+    elif action == 'submit':
+        total = parse_files(open(BEEMIND_DATA_FILE))
+        total = total[goal]
         total /= 3600
-        post_data(BEEMIND_AUTH_TOKEN, BEEMIND_GOAL, '{:.4}'.format(total), "from command line")
-        unlink(BEEMIND_DATA_FILE)
-        
+        post_data(BEEMIND_AUTH_TOKEN, goal, '{:.4}'.format(total), "from command line")
+        remove_goal_data(BEEMIND_DATA_FILE, goal)
 
 if __name__ == '__main__':
     from sys import argv
